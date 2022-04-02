db8b383106250a115bd919c6b70a28290ffad4a0
vaishnavikawde <vaishnavikawde2gmail.com>
2022-04-02 15:14:29 +0530

    initial change as a comment

--- testCases/test_registration.py
+++ testCases/test_registration.py
@@ -48,7 +48,7 @@ class Test_003_AddCustomer:
         self.driver.close()
 
 
-
+# this is random code
 # this code generate randome 8 char for email address
 def random_generator(size=8, chars=string.ascii_lowercase+string.digits):
     return ''.join(random.choice(chars) for x in range(size))
